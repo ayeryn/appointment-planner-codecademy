89d2c6d99061a4c37b6ce8d3dace02973f426616
Eryn Li <erynli@utexas.edu>
2025-06-17 18:24:10 -0500

    update App.js with necessary hooks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import React from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
+import React, { useState } from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  function addAppointment(name, contact, date, time) {
+    const newAppointment = {
+      name: name,
+      contact: contact,
+      date: date,
+      time: time,
+    };
 
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root/> }>
-      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
-    </Route>
-  ));
-  
-  return (
-    <RouterProvider router={router}/>
+    setAppointments([...appointments, newAppointment]);
+  }
+
+  function addContact(name, phone, email) {
+    const newContact = {
+      name: name,
+      phone: phone,
+      email: email,
+    };
+
+    setContacts([...contacts, newContact]);
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
+        <Route
+          path={ROUTES.CONTACTS}
+          element={<ContactsPage contacts={contacts} addContact={addContact} />}
+        />
+        <Route
+          path={ROUTES.APPOINTMENTS}
+          element={
+            <AppointmentsPage
+              appointments={appointments}
+              contacts={contacts}
+              addAppointment={addAppointment}
+            />
+          }
+        />
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
