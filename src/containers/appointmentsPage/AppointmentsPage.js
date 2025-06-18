715cda531cd5df9697bfbd3f1b516eeb62ba0531
Eryn Li <erynli@utexas.edu>
2025-06-17 19:17:26 -0500

    Update appt page

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,28 +3,48 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = ({
+  appointments,
+  contacts,
+  addAppointment,
+}) => {
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+
+    addAppointment(name, contact, date, time);
+    setName("");
+    setContact("");
+    setDate("");
+    setTime("");
+  };
+
+  const props = {
+    name,
+    setName,
+    contact,
+    setContact,
+    date,
+    setDate,
+    time,
+    setTime,
+    handleSubmit,
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm props={props} />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList appointments={appointments} />
       </section>
     </div>
   );
